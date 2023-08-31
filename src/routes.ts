9952927c7c5b9e21ccbab187068ea1900e2967b0
CaioSouzaAds <caio.cesarads@gmail.com>
2023-08-31 11:19:17 -0300

    created models category, product, order, item and migrations para bd postgre

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,12 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
+router.get("/", (req: Request, res: Response) => {
+  return res.json({
+    nome: "rota /",
+  });
+});
+
 router.get("/teste", (req: Request, res: Response) => {
   return res.json({
     nome: "Caio",
